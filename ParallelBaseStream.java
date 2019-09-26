95a75f0e3d32760465d4dd54bcf17e65a95656c2
RajPenjarla-git <rajkpenjarla@gmail.com>
2019-09-26 23:51:18 +0530

    Updated code for isParallel() method

--- ParallelBaseStream.java
+++ ParallelBaseStream.java
@@ -10,10 +10,13 @@ public class ParallelBaseStream {
 		IntStream normalStream = IntStream.rangeClosed(1, 5);
 		System.out.println("Normal Streaming");
 		normalStream.forEach(System.out::println);
+		System.out.println("Is Stream Running in Parallel mode: " + normalStream.isParallel());
 		
 		//Parallel Streaming
 		IntStream parallelStream = IntStream.rangeClosed(6, 10);
 		System.out.println("Parallel Streaming");
 		parallelStream.parallel().forEach(System.out::println);
+		System.out.println("Is Stream Running in Parallel mode: " + parallelStream.isParallel());
+		
  	}
 }
