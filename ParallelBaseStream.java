304523cee1d6fcb20fd9bdc342edda488a2f5248
RajPenjarla-git <rajkpenjarla@gmail.com>
2019-09-26 23:32:13 +0530

    BaseSteam.Parallel() example

---
+++ ParallelBaseStream.java
@@ -0,0 +1,19 @@
+import java.util.stream.IntStream;
+
+
+public class ParallelBaseStream {
+
+	
+	public static void main(String[] args) {
+		
+		//Normal Streaming
+		IntStream normalStream = IntStream.rangeClosed(1, 5);
+		System.out.println("Normal Streaming");
+		normalStream.forEach(System.out::println);
+		
+		//Parallel Streaming
+		IntStream parallelStream = IntStream.rangeClosed(6, 10);
+		System.out.println("Parallel Streaming");
+		parallelStream.parallel().forEach(System.out::println);
+ 	}
+}
