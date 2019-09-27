0ecf9cce8a478e090df96d803dcd38dbd9889730
RajPenjarla-git <rajkpenjarla@gmail.com>
2019-09-27 20:13:44 +0530

    Optional Class methods example

--- OptionalClass.java
+++ OptionalClass.java
@@ -18,11 +18,20 @@ public class OptionalClass {
 			System.out.println("string[5] is: "+withOptionalClass);
 		}
 		
-		//Without Optional Class
+		//Setting value for Optional at index 4
+		 string[4] = "Raj";
+		 withOptionalClass = Optional.of(string[4]); //is not null
+		 System.out.println("Optional.get() method returns value: "+withOptionalClass.get());
+		 System.out.println("Optional.isPresent() method returns true if value present: "+withOptionalClass.isPresent());
+		 System.out.println("Optional.hashCode() method returns hashcode: "+withOptionalClass.hashCode());
+				
+		 
+		 //Without Optional Class
 		System.out.println("-------Without Optional Class--------");
-		String withoutOptionalClass = string[4];
+		String[] str = new String[10];
+		String withoutOptionalClass = str[4];
 		System.out.println("string[4] is: "+ withoutOptionalClass);
-		System.out.println("string[4].toUpperCase() will throw NullPointerException :");
-		withoutOptionalClass = string[4].toUpperCase();
+		System.out.println("string[4].toUpperCase() will throw NullPointerException :" +withoutOptionalClass);
+		withoutOptionalClass = str[4].toUpperCase();
 	}
 }
